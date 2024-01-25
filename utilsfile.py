85a4d6a6a7d6ff9ff3881dda82402f3eb1da09b9
William Paul <williamjamespaul1@gmail.com>
2024-01-25 09:31:58 +0100

    Added CPU Ship, Added angle calculation for CPU ship path

--- utilsfile.py
+++ utilsfile.py
@@ -4,3 +4,9 @@ import pygame
 def scale_image(img, factor):
     size = round(img.get_width() * factor), round(img.get_height() * factor)
     return pygame.transform.scale(img, size)
+
+#take an image and rotate it
+def blit_rotate_center(win, image, top_left, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center=image.get_rect(topleft = top_left).center)
+    win.blit(rotated_image, new_rect.topleft)
